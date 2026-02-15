c35aeaca7d5a8b811d3967adf8f462ffbdb89453
Sheikh Asif <shiekhasif2005@gamil.com>
2026-02-15 10:18:57 +0530

    Reverse-Vowel -String

---
+++ ReverseVowelInString.cpp
@@ -0,0 +1,26 @@
+// Name: reverse Vowel in string
+// level: easy
+// plateform leedcode :https://leetcode.com/problems/reverse-vowels-of-a-string/
+
+// code 
+class Solution {
+public:
+    bool isVowel(char ch){
+        return (ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u'||
+                 ch=='A' || ch=='E' || ch=='I' || ch=='O' || ch=='U');
+    }
+    string reverseVowels(string s) {
+        int i=0;
+        int j=s.length()-1;
+        while(i<j){
+            if(!isVowel(s[i])) i++;
+            else if(!isVowel(s[j])) j--;
+            else{
+                swap(s[i],s[j]);
+                i++;
+                j--;
+            }    
+        }
+        return s;
+    }
+};
