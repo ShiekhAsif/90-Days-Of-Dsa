ad95ec3eb43f004b1313cbfa5bd3d493be459651
Sheikh Asif <shiekhasif2005@gamil.com>
2026-02-20 14:22:14 +0530

    BestTimeToBuyAndSellStock

---
+++ Arrays/BestTimeToBUyAnsSell.cpp
@@ -0,0 +1,17 @@
+// problem:Best Time to Buy and Sell Stock
+// difficult:easy
+// link:https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+// code---
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int maxProfit=0;
+        int minPrice=INT_MAX;
+        for(int i=0;i<prices.size();i++){
+            minPrice=min(minPrice,prices[i]);
+            maxProfit=max(maxProfit, (prices[i]-minPrice));
+        }
+        return maxProfit;
+    }
+};
